e1f00243d14cb47defa1cf5efb350ea96b2700e1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-07-20 21:09:40 -0400

    inherit system

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {};
+        pkgs = import nixpkgs { inherit system; };
       in {
         packages = rec {
           nfc = pkgs.haskellPackages.callPackage ./nix/nfc.nix {};
