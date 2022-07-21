11154b6bcd1744f752a2ed2ef99d7a672f9d0583
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-07-20 20:48:01 -0400

    pin nixpkgs version

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "Haskell bindings to libnfc";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
